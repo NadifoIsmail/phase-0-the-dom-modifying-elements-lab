91784fef08c5562b98fef55d01b5ba78361322f8
Nadifo Ismail <nadifoismail80@gmail.com>
2024-10-18 08:27:55 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+//remove the main tag with an id of main
+main.remove();
+
+//create a h1 element
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Nadi is the champion";
